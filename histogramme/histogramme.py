b5a67aa5cba591609ed97f4d58bcf63c87de3085
SALIM <saissi1701@gmail.com>
2022-05-30 10:22:44 +0200

    sift descripteur

--- histogramme/histogramme.py
+++ histogramme/histogramme.py
@@ -8,11 +8,11 @@ x=Axes3D(fig)
 import sys
 from tqdm import tqdm
 cpt=0
-for i in tqdm(os.listdir(r"C:\Users\SALIM\PycharmProjects\evalproject\\"+sys.argv[1]+"\\image")):
-    img = cv.imread(r'C:\Users\SALIM\PycharmProjects\evalproject\\'+sys.argv[1]+"\\image\\"+i,0)
+for i in tqdm(os.listdir(sys.argv[1]+"\\image")):
+    img = cv.imread(sys.argv[1]+"\\image\\"+i,0)
     hist = cv.calcHist([img],[0],None,[256],[0,256])
 
     x.bar(left=range(256),height= [x[0] for x in hist],zs=cpt,zdir="y")
     cpt+=1
 
-plt.savefig(r"C:\Users\SALIM\PycharmProjects\evalproject\histogramme\\"+sys.argv[1]+".png")
+plt.savefig(r"C:\Users\SALIM\PycharmProjects\evalproject\histogramme\\"+sys.argv[2]+".png")
